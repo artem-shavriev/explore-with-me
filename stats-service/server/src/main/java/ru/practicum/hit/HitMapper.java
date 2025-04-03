6d83345e8acf8802a221e2c97b5fbf76badee82b
Artem Shavriev <artem-shavriev@yandex.ru>
2025-04-03 18:16:53 +0300

    Исправить стиль.

--- stats-service/server/src/main/java/ru/practicum/hit/HitMapper.java
+++ stats-service/server/src/main/java/ru/practicum/hit/HitMapper.java
@@ -12,28 +12,10 @@ import java.time.format.DateTimeFormatter;
 @Component
 @NoArgsConstructor
 public class HitMapper {
-    DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public EndpointHit mapToHitDto(Hit hit) {
-        if (hit == null) {
-            throw new NotFoundException("Hit не может быть равен null");
-        }
-        EndpointHit endpointHit = new EndpointHit();
-
-        endpointHit.setIp(hit.getIp());
-        endpointHit.setApp(hit.getApp());
-        endpointHit.setUri(hit.getUri());
-
-
-        String dateTimeString = hit.getTimestamp().format(FORMATTER);
-
-        endpointHit.setTimestamp(dateTimeString);
-
-        return endpointHit;
-    }
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public Hit mapToHit(EndpointHit dto) {
-        if (dto== null) {
+        if (dto == null) {
             throw new NotFoundException("HitDto не может быть равен null");
         }
         Hit hit = new Hit();
@@ -42,7 +24,7 @@ public class HitMapper {
         hit.setApp(dto.getApp());
         hit.setUri(dto.getUri());
 
-        LocalDateTime dateTime = LocalDateTime.parse(dto.getTimestamp(), FORMATTER);
+        LocalDateTime dateTime = LocalDateTime.parse(dto.getTimestamp(), formatter);
 
         hit.setTimestamp(dateTime);
 
--- stats-service/server/src/main/java/ru/practicum/stats/StatsServiceImpl.java
+++ stats-service/server/src/main/java/ru/practicum/stats/StatsServiceImpl.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.HitRepository;
 import ru.practicum.ViewStats;
-import ru.practicum.hit.model.Hit;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -24,29 +22,7 @@ public class StatsServiceImpl implements StatsService {
 
         LocalDateTime timeStart = LocalDateTime.parse(start, formatter);
         LocalDateTime timeEnd = LocalDateTime.parse(end, formatter);
-        List<Hit> hits = new ArrayList<>();
 
-        /*if (unique == true) {
-            hits = repository.findUniqueHitsByUrisInPeriod(timeStart, timeEnd, uris);
-
-            List<ViewStats> statsList = new ArrayList<>();
-            hits.forEach(hit -> {
-                ViewStats viewStats = new ViewStats(hit.getApp(), hit.getUri(),
-                        repository.countByAppAndUriAndIp(hit.getApp(), hit.getUri(), hit.getIp()));
-                statsList.add(viewStats);
-            });
-
-            return statsList;
-        } else {
-            hits = repository.findHitsByUrisInPeriod(timeStart, timeEnd, uris);
-            List<ViewStats> statsList = new ArrayList<>();
-            hits.forEach(hit -> {
-                ViewStats viewStats = new ViewStats(hit.getApp(), hit.getUri(),
-                        repository.countByAppAndUri(hit.getApp(), hit.getUri()));
-                statsList.add(viewStats);
-            });
-            return statsList;
-        }*/
         if (unique == true) {
             List<Object[]> stats = repository.findUniqueStatsByUrisInPeriod(timeStart, timeEnd, uris);
             return stats.stream()
